e7501dfe8efd53c0a97b1894d34304efd06fadbf
mostafa fallaha <moostafafallaha@gmail.com>
2024-10-29 12:27:45 +0200

    add data version 1.0

--- dvc_commands.sh
+++ dvc_commands.sh
@@ -1,10 +1,37 @@
 #!/bin/bash
+
+# Load the version from .env file
+source /home/mostafa/Desktop/FSD\ Projects/big_mart/.env
+version=$VERSION
+
+# Function to increment the version
+increment_version() {
+    major=${version%%.*}
+    minor=${version##*.}
+    if [ "$minor" -eq 9 ]; then
+        major=$((major + 1))
+        minor=0
+    else
+        minor=$((minor + 1))
+    fi
+    echo "$major.$minor"
+}
+
+# Update version
+new_version=$(increment_version)
+
+# Update .env file with the new version
+sed -i "s/VERSION=$version/VERSION=$new_version/" /home/mostafa/Desktop/FSD\ Projects/big_mart/.env
+
+# DVC and Git commands
 cd '/home/mostafa/Desktop/FSD Projects/big_mart'
 dvc add dags/data/dvc_df.csv
 dvc add dags/data/df.csv
 git add .
-git commit -m "add data"
+git commit -m "add data version $new_version"
+git tag -a "v$new_version" -m "Version $new_version"
 dvc push
 git push
+git push origin --tags
 
-# continue the process to add training model and these stuffw
+echo "---- Done: Version $new_version ----"
